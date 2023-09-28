30d598f93433b81a43b29d743ce4e2272b25f367
MorgynKass <morgynkass@gmail.com>
2023-09-27 18:46:01 -0600

    finished js 2

--- script.js
+++ script.js
@@ -3,5 +3,28 @@ let date = document.querySelector("#date");
 let currentYear = new Date().getFullYear();
 let year = document.querySelector("#year");
 
+let cardList = document.querySelector('.cards');
+let cards = [];
+let cardObject = {
+    title: "Html Project",
+    urlLink: "Project_1/project1.html",
+};
+
+cards.push(cardObject);
+console.log(cards);
+
+cards.map((item) => {
+    cardList.innerHTML += `<div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+        <h2>${item.title}</h2>
+      </div>
+      <div class="card-back">
+        <a href="${item.urlLink}">Project Link</a>
+      </div>
+    </div>
+  </div>`
+});
+
 date.textContent = currentDate;
 year.textContent = currentYear;
